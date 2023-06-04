eca58a8c8e4b58bd064686afb624bb7ef514d441
davidgm-92 <david.gm.92@gmail.com>
2023-06-04 14:14:40 +0200

    First version

---
+++ scripts/content.js
@@ -0,0 +1,36 @@
+var obs = new MutationObserver(function (mutations) {
+  const videoList = document.getElementsByTagName("ytd-grid-video-renderer");
+
+  for (let i = 0; i < videoList.length; i++) {
+    let hide = false;
+    let author = videoList[i].querySelector(
+      ".yt-simple-endpoint.style-scope.yt-formatted-string"
+    ).text;
+    if (author === "The Wild Project") {
+      let length = videoList[i]
+        .querySelector(
+          "#text.style-scope.ytd-thumbnail-overlay-time-status-renderer"
+        )
+        .innerText.toString();
+      let lengthDivided = length.split(":").map((x) => parseInt(x));
+
+      let time = 0;
+      if (lengthDivided.length == 2) {
+        time = lengthDivided[0] * 60 + lengthDivided[1];
+      }
+      if (lengthDivided.length == 3) {
+        time =
+          lengthDivided[0] * 60 * 60 + lengthDivided[1] * 60 + lengthDivided[2];
+      }
+      if (time < 3600) {
+        videoList[i].style.display = "none";
+      }
+    }
+  }
+});
+obs.observe(document.body, {
+  childList: true,
+  subtree: true,
+  attributes: false,
+  characterData: false,
+});
